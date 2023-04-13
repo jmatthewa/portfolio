ec7f0c194b2a0fb14d3641cc1e7599f2a0f43ea4
jmatthewa <jamesm.atanacio@gmail.com>
2023-04-13 12:53:20 -0700

    Update about section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,12 +1,13 @@
 <template>
-    <div>
-        <div>
+    <div name="about" class="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white
+        ">
+        <div class="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
 
-            <div>
-                <p>About</p>
+            <div class="pb-8">
+                <p class="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
 
-            <p>
+            <p class="text-xl mt-20">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias soluta id aliquam velit a tenetur eos eligendi consectetur odit quam dignissimos, sit laboriosam qui dolores quis ad dolorem repudiandae.
 
             </p>
