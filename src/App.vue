f830d6dfe5c1d3fa13627e1699172c05578007dd
jmatthewa <jamesm.atanacio@gmail.com>
2023-04-18 03:40:19 -0700

    Add project section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <router-view/>
+  <div class="home">
+
+    <NavbarSection />
+    <HeroSection />
+    <AboutSection />
+    <ProjectsSection />
+    <SocialLinks />
+
+  </div>
 </template>
 
+<script>
+// @ is an alias to /src
+
+import NavbarSection from '@/components/NavbarSection.vue'
+import HeroSection from '@/components/HeroSection.vue';
+import SocialLinks from '@/components/SocialLinks.vue';
+import AboutSection from '@/components/AboutSection.vue';
+import ProjectsSection from '@/components/ProjectsSection.vue'
+
+export default {
+  components: {
+   NavbarSection,
+   HeroSection,
+   SocialLinks,
+   AboutSection,
+   ProjectsSection
+  }
+}
+
+</script>
 <style>
 
 </style>
---
+++ src/components/ProjectsSection.vue
@@ -0,0 +1,53 @@
+<template>
+      <div name="project" class="bg-gradient-to-b from-black to bg-gray-800 w-full text-white md:h-screen">
+        <div class="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div class="pb-8">
+                  <p class="text-4xl font-bold inline border-b-4 border-gray-500">Project</p>
+                  <p class="py-6">Check out some of my project right here</p>
+            </div>
+
+            <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+
+                  <div  v-for="project in projects" :key="project.id" class="shadow-md shadow-gray-600 rounded-lg">
+                        <img :src="project.src" alt="" class="rounded-md duration-200 hover:scale-105">
+
+                        <div class="flex items-center justify-center">
+                              <a :href="project.live" target="_blank" class="w-1/2 px-6 py-3 m-4 duration-200 hover: scale-105"><button>Demo</button></a>
+                              <a :href="project.code" target="_blank" class="w-1/2 px-6 py-3 m-4 duration-200 hover: scale-105"><button>Code</button></a>
+                              
+                              
+                        </div>
+                  </div>
+            </div>
+        </div>
+      </div>
+</template>
+<script>
+
+import project from '../assets/project.png'
+import project2 from '../assets/project2.png'
+
+export default {
+    setup() {
+
+      const projects = [
+            {
+                  id: 1,
+                  src: project
+            },
+            {
+                  id: 2,
+                  src: project2,
+                  code: 'https://github.com/jmatthewa/the-strawhat-fit',
+                  live: 'https://strawhatfit.pages.dev/'
+            },
+            {
+                  id: 3,
+                  src: project
+            }
+      ]
+
+      return { projects }
+    }
+}
+</script>
