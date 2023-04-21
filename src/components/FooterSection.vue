cf6188ff7b4b2e981ee5dd04e7e12c6a76c5124a
jmatthewa <jamesm.atanacio@gmail.com>
2023-04-21 05:21:42 -0700

    Add footersection

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,10 +1,32 @@
 <template>
-    <div>
-        Footer
+    <div class="h-34 flex flex-col items-center justify-center text-white bg-gradient-to-b from-gray-800 to-black">
+        <div class="flex flex-col ">
+            <h1 class="flex text-5xl p-5 font-signature items-center justify-center">Matt</h1>
+
+            <div class="flex items-center justify-between">
+                <a href="https://www.linkedin.com/in/james-matthew-atanacio-0b0882173/"  target="_blank"> <fa :icon="['fab','linkedin']" size="xl"/></a>
+                <a href="https://github.com/jmatthewa"   target="_blank"><fa :icon="['fab','github']" size="xl"/></a>
+                <a href="mailto:matthew_1147@yahoo.com"  target="_blank"><fa :icon="['fas','envelope']" size="xl"/></a>
+            </div>
+
+
+          
+        </div>
+        <p class="flex items-center justify-center p-10 text-xs">created by: James Matthew &copy; {{ new Date().getFullYear() }}</p>
     </div>
 </template>
 <script>
 export default {
-    
+    setup() {
+
+        // const date = new Date();
+
+        // return {
+
+        //     date
+        // }
+    }
+
+
 }
 </script>
