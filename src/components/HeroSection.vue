e3d482d121b37d9d3b007b863caa6c02d9d4b8b9
jmatthewa <jamesm.atanacio@gmail.com>
2023-04-24 05:40:48 -0700

    Update v-motion

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,7 @@
         <div class="mt-20 max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 sm:flex-row sm:mt-0">
             <div  class="flex flex-col justify-center h-full">
              
-                    <h2 v-motion-slide-visible-left
+                    <h2 v-motion-fade
                     class="duration-500 text-4xl  sm:text-7xl  font-bold text-white">
                         I'm a
                    </h2>
@@ -18,13 +18,13 @@
                 
          
                 
-                    <p v-motion-slide-visible-left class="duration-500 text-gray-500 py-4 max-w-md">
+                    <p  v-motion-fade class="duration-500 text-gray-500 py-4 max-w-md">
                         Hello and welcome! My name is James Matthew, and I am a web developer building user-friendly, responsive, and visually appealing websites and web applications
                     </p>
                     
       
                     <div>
-                        <button v-motion-slide-visible-bottom @click="scroll('project')" class="group text-white w-fit px-6 py-3 my-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+                        <button  v-motion-fade @click="scroll('project')" class="group text-white w-fit px-6 py-3 my-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
                             Portfolio 
                    
                             <fa :icon="['fas', 'angles-right']" class="group-hover:rotate-90 duration-300 ml-1"/>
@@ -36,7 +36,7 @@
 
             
                 <div>
-                    <img v-motion-slide-visible-right :src="HeroImage" alt="" class="duration-500 hidden md:block mx-auto w-2/3 sm:w-full">
+                    <img  v-motion-fade :src="HeroImage" alt="" class="duration-500 hidden md:block mx-auto w-2/3 sm:w-full">
                 </div>
       
         </div>
@@ -102,9 +102,7 @@ export default {
 </script>
 <style>
 
-span.cursor.typing {
-    animation: none;
-}
+
 
 span.cursor {
     background-color: #fff;
